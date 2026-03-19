8ba16cd1003c5d827aafb6fd5b73d252c2f0ff43
Krishal <krishalbatra18@gmail.com>
2026-03-19 14:00:26 +0530

    updated smooth scroll

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import HeroSection from '../sections/HeroSection'
-import NavBar from '../components/NavBar'
-import MessageSection from '../sections/MessageSection'
-import NutritionSection from '../sections/NutritionSection'
-import FlavorSection from '../sections/FlavorSection'
-import BenefitSection from '../sections/BenefitSection'
-import TestimonialSection from '../sections/TestimonialSection'
-import FooterSection from '../sections/FooterSection'
-import { useGSAP } from '@gsap/react'
+import React from "react";
+import HeroSection from "../sections/HeroSection";
+import NavBar from "../components/NavBar";
+import MessageSection from "../sections/MessageSection";
+import NutritionSection from "../sections/NutritionSection";
+import FlavorSection from "../sections/FlavorSection";
+import BenefitSection from "../sections/BenefitSection";
+import TestimonialSection from "../sections/TestimonialSection";
+import FooterSection from "../sections/FooterSection";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 const Home = () => {
-   useGSAP(() => {
-    ScrollSmoother.create({
-      smooth: 3,
-      effects: true,
-    });
+  useGSAP(() => {
+    if (window.innerWidth > 768) {
+      requestIdleCallback(() => {
+        ScrollSmoother.create({
+          smooth: 1.2,
+          effects: false,
+        });
+      });
+    }
   });
   return (
     <main>
       <NavBar />
-      
+
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <section id="home">
@@ -47,7 +51,7 @@ const Home = () => {
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
 export default Home;
